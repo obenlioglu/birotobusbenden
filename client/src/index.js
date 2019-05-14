411c244d0b08e164619d69b55d17b381f255a8d5
Osman <osman.benlioglu@gmail.com>
2019-05-14 16:04:07 +0300

    token updated

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,15 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-    uri: process.env.REACT_APP_HTTP_URI
+    uri: process.env.REACT_APP_HTTP_URI,
+    request: async operation => {
+        const token = await localStorage.getItem('token');
+        operation.setContext({
+          headers: {
+            authorization: token ? `Bearer ${token}` : ''
+          }
+        });
+       }
 });
 
 ReactDOM.render(
